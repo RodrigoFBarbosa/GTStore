907a9749e548c9b5c76bd5a4842928a0aeeb11eb
Rodrigo <rodrigobarbosa.p99@gmail.com>
2024-10-13 20:33:37 -0300

    fix price

--- scrypt.js
+++ scrypt.js
@@ -9,15 +9,15 @@ const products = [
         image: "assets/imgs/sale.png"
     },
     {
-        name: "Camisa Oversized KING",
-        description: "Sinta-se no topo com a nossa camisa KING, conforto e estilo no seu treino.",
-        price: "R$ 100,00",
-        image: "assets/imgs/KING C.jpg"
+        name: "Camisa Oversized ZEUS",
+        description: "Sinta-se no topo do Olimpo com a nossa camisa ZEUS, conforto e estilo no seu treino.",
+        price: "R$ 89,90",
+        image: "assets/imgs/ZEUS C P.jpg"
     },
     {
         name: "Camisa Oversized TRUE NATTY",
         description: "Para aqueles que são 100% TRUE NATTY e querem mostrar isso no estilo.",
-        price: "R$ 100,00",
+        price: "R$ 89,90",
         image: "assets/imgs/NATTY C B.jpg"
     }
 ];
@@ -82,9 +82,15 @@ const catalogProducts = [
         sizeImage: "assets/imgs/Sizes.jfif",
     },
     {
-        name: "Camisa oversized KING",
+        name: "Camisa oversized ZEUS preta",
         price: "R$ 89,90",
-        image: "assets/imgs/KING C.jpg",
+        image: "assets/imgs/ZEUS C P.jpg",
+        sizeImage: "assets/imgs/Sizes.jfif",
+    },
+    {
+        name: "Camisa oversized ZEUS branca",
+        price: "R$ 89,90",
+        image: "assets/imgs/ZEUS C B.jpg",
         sizeImage: "assets/imgs/Sizes.jfif",
     },
     {
@@ -100,6 +106,12 @@ const catalogProducts = [
         sizeImage: "assets/imgs/Sizes.jfif",
     },
     {
+        name: "Camisa oversized KING",
+        price: "R$ 89,90",
+        image: "assets/imgs/KING C.jpg",
+        sizeImage: "assets/imgs/Sizes.jfif",
+    },
+    {
         name: "Camisa oversized O MESMO DE ONTEM",
         price: "R$ 89,90",
         image: "assets/imgs/O MESMO DE ONTEM C.jpg",
@@ -111,95 +123,65 @@ const catalogProducts = [
         image: "assets/imgs/QUIMICA C P.jpg",
         sizeImage: "assets/imgs/Sizes.jfif",
     },
-    {
-        name: "Camisa oversized ZEUS preta",
-        price: "R$ 89,90",
-        image: "assets/imgs/ZEUS C P.jpg",
-        sizeImage: "assets/imgs/Sizes.jfif",
-    },
-    {
-        name: "Camisa oversized ZEUS branca",
-        price: "R$ 89,90",
-        image: "assets/imgs/ZEUS C B.jpg",
-        sizeImage: "assets/imgs/Sizes.jfif",
-    }
 ];
 
-let productsLoaded = 0; 
+let productsLoaded = catalogProducts.length; // Inicialmente, carrega todos os produtos
 
-
+// Renderiza um único produto
 function renderProduct(product) {
     const productGrid = document.getElementById('product-grid');
     const productCard = `
         <div class="product-card">
-    <img src="${product.image}" alt="${product.name}">
-    <h3>${product.name}</h3>
-    <p>${product.price}</p>
-    <button class="btn" onclick="openModal('${product.name}', '${product.image}', '${product.sizeImage}',)">Mais detalhes</button>
-    <button class="btn secondary" onclick="window.open('https://wa.me/5511954619955?text=Estou%20interessado%20neste%20produto%20${product.name}', '_blank')">
-        Comprar no WhatsApp <i class='bx bxl-whatsapp'></i>
-    </button>
-</div>
+            <img src="${product.image}" alt="${product.name}">
+            <h3>${product.name}</h3>
+            <p>${product.price}</p>
+            <button class="btn" onclick="openModal('${product.name}', '${product.image}', '${product.sizeImage}')">Mais detalhes</button>
+            <button class="btn secondary" onclick="window.open('https://wa.me/5511954619955?text=Estou%20interessado%20neste%20produto%20${product.name}', '_blank')">
+                Comprar no WhatsApp <i class='bx bxl-whatsapp'></i>
+            </button>
+        </div>
     `;
     productGrid.innerHTML += productCard;
 }
 
-// Função para abrir o modal
-function openModal(name, image, sizeImage,) {
-    document.getElementById('modalProductName').textContent = name;
-    document.getElementById('modalProductImage').src = image;
-    document.getElementById('modalSizesImage').src = sizeImage;
-
-    const modal = document.getElementById('productModal');
-    modal.style.display = "block";
-
-    
-    document.querySelector('.close').onclick = function() {
-        modal.style.display = "none";
-    }
-
-  
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    }
+// Renderiza todos os produtos no carregamento inicial da página
+function renderAllProducts() {
+    catalogProducts.forEach(product => renderProduct(product));
+    updateButtonsVisibility();
 }
 
-
+// Carrega mais produtos (3 de cada vez)
 function loadMoreProducts() {
     const productsToLoad = catalogProducts.slice(productsLoaded, productsLoaded + 3);
     productsToLoad.forEach(product => renderProduct(product));
     productsLoaded += productsToLoad.length;
 
-   
     updateButtonsVisibility();
 }
 
-
+// Remove os últimos 3 produtos carregados
 function loadLessProducts() {
     const productGrid = document.getElementById('product-grid');
     const cardsToRemove = Array.from(productGrid.children).slice(-3);
     cardsToRemove.forEach(card => card.remove());
     productsLoaded -= cardsToRemove.length;
 
-    
     updateButtonsVisibility();
 }
 
-
+// Atualiza a visibilidade dos botões de "Carregar mais" e "Carregar menos"
 function updateButtonsVisibility() {
     const loadMoreBtn = document.getElementById('loadMoreBtn');
     const loadLessBtn = document.getElementById('loadLessBtn');
 
-    
+    // Esconde o botão "Carregar mais" quando todos os produtos estão carregados
     if (productsLoaded >= catalogProducts.length) {
         loadMoreBtn.style.display = 'none';
     } else {
         loadMoreBtn.style.display = 'block';
     }
 
-    
+    // Mostra o botão "Carregar menos" apenas quando mais de 3 produtos estão carregados
     if (productsLoaded > 3) {
         loadLessBtn.style.display = 'block';
     } else {
@@ -207,12 +189,32 @@ function updateButtonsVisibility() {
     }
 }
 
-
+// Carrega todos os produtos ao abrir a página
 window.onload = function() {
-    loadMoreProducts();
+    renderAllProducts();
+};
+
+// Função para abrir o modal
+function openModal(name, image, sizeImage) {
+    document.getElementById('modalProductName').textContent = name;
+    document.getElementById('modalProductImage').src = image;
+    document.getElementById('modalSizesImage').src = sizeImage;
+
+    const modal = document.getElementById('productModal');
+    modal.style.display = "block";
+
+    document.querySelector('.close').onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
 }
 
-
+// Adiciona os eventos para os botões de "Carregar mais" e "Carregar menos"
 document.getElementById('loadMoreBtn').addEventListener('click', function(event) {
     event.preventDefault();
     loadMoreProducts();
@@ -222,8 +224,3 @@ document.getElementById('loadLessBtn').addEventListener('click', function(event)
     event.preventDefault();
     loadLessProducts();
 });
-
-function changeImage(imageId, imagePath) {
-    document.getElementById(imageId).src = imagePath;
-}
-
